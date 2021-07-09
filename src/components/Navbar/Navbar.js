5a26feb4f19c0f4cee48aaa4979446a15d77ef18
animesh103 <animeshraj4697@gmail.com>
2021-07-09 23:15:52 +0530

    added navbar

---
+++ src/components/Navbar/Navbar.js
@@ -0,0 +1,17 @@
+import React from "react";
+import "./navbar.css";
+
+function Navbar() {
+	return <div>
+		<nav> 
+		<div class="companyLogoContainer">
+			<div class="logo">
+				<img src="../../assets/images/logo.png" alt="logo" />
+			</div>
+			<h1>untangledchat</h1>
+		</div>
+		</nav>
+	</div>;
+}
+
+export default Navbar;
